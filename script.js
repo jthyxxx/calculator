0ac0b5efa9092649c6b7cc80582fb58ad0a3dda8
Jethro Baring <jyty.dev@gmail.com>
2022-06-27 23:43:26 +0800

    all function done

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const buttons = document.querySelectorAll('button')
+const buttons = document.querySelectorAll('.symbolButton')
 const upper = document.querySelector('.upper')
 const lower = document.querySelector('.lower')
 const currentOpe = document.querySelector('.currentOperator')
@@ -7,118 +7,62 @@ const currentOpe = document.querySelector('.currentOperator')
 let calculation = [];
 let total = 0;
 let currentNumber = 0;
-let currentN;
 let currentOperator = ""
-lower.innerText = total
-
-let operators = ['+','-','×','÷','=','.','del','allclear']
-
-let displayUpdate = () => {
-    if(calculation.length > 17){
-        upper.innerText = (Number(calculation.join(""))).toExponential(2)
-    }
-    else{
-        upper.innerText = calculation.join("")
-    }
-    currentOpe.innerText = currentOperator
-}
-
-let displayTotal = () => {
-    let totalLength = Array.from(total.toString())
-    let ind = totalLength.indexOf('.')
-    let decimals = [...totalLength.slice(ind+1)]
-    if(decimals.length >= 3){
-        if(totalLength.length > 17){
-            lower.innerText = total.toExponential(2)
-        }
-        else{
-            lower.innerText = total.toFixed(2)
-        }
-    }else{
-        if(totalLength.length > 17){
-            lower.innerText = total.toExponential(2)
-        }
-        else{
-            lower.innerText = total
-        }
-    }
-    
-}
-
-let erase = () => {
-    if(calculation.length <= 1){
-        calculation.pop()
-        upper.innerText = 0
-    }else{
-        calculation.pop()
-        currentNumber = Number(calculation.join(""))
-        displayUpdate()
-    }
-}
-
-let eraseAll = () => {
-    calculation = []
-    currentNumber = 0
-    total = 0
-    setZero()
-}
-
-let setZero = () => {
-    upper.innerText = 0
-    lower.innerText = 0
-}
 
 buttons.forEach((button)=>{
+
     button.addEventListener('mousedown', ()=>{
         button.style.border = '3px solid #C996CC'
         button.style.fontSize = '2.8rem'
         
-        
-
         if(operators.includes(button.value)){
+
             if(button.value === '+'){
                 solve()
-                currentOperator = '+'
-                calculation = []
-                currentNumber = 0
+                set('+')
             }else if(button.value === '-'){
                 solve()
-                currentOperator = '-'
-                calculation = []
-                currentNumber = 0
+                set('-')
             }else if(button.value === '×'){
                 solve()
-                currentOperator = '×'
-                calculation = []
-                currentNumber = 1
+                set('×')
             }else if(button.value === '÷'){
                 solve()
-                currentOperator = '÷'
-                calculation = []
-                currentNumber = 1
+                set('÷')
             }else if(button.value === '='){
                 solve()
-                currentOperator = ""
+                set('')
+                currentNumber = 0
             }else if(button.value === '.'){
                 calculation.push(button.value)
+                displayUpdate()
             }else if(button.value === 'del'){
                 erase()
-            }else if(button.value === 'allclear'){
+            }else{
                 eraseAll()
+                    currentOpe.innerText = ""
             }
+
         }
         
         else{
+
+            if(button.value === '00'){
+                calculation.push('0')
+            }
+
             calculation.push(Number(button.value))
             currentNumber = Number(calculation.join(""))
             if(currentOperator == ""){
                 currentOperator = ""
             }
+
+            displayUpdate()
         }
         
         
         
-        displayUpdate()
+       
         
     })
     
@@ -135,11 +79,90 @@ buttons.forEach((button)=>{
 }
 )
 
+if(total != 0){
+    lower.innerText = total
+}
+let operators = ['+','-','×','÷','=','.','del','allclear']
+
+let displayUpdate = () => {
+
+    if(calculation.length > 17){
+        upper.innerText = (Number(calculation.join(""))).toExponential(2)
+    }
+    else{
+        upper.innerText = calculation.join("")
+    }
+    currentOpe.innerText = currentOperator
+}
+
+let displayTotal = () => {
+
+    let totalLength = Array.from(total.toString())
+    let ind = totalLength.indexOf('.')
+    let decimals;
+    if(ind != -1){
+        decimals = [...totalLength.slice(ind+1)]
+        if(decimals.length >= 3){
+
+            if(totalLength.length > 17){
+                lower.innerText = total.toExponential(2)
+            }
+            else{
+                lower.innerText = total.toFixed(2)
+            }
+            
+        }else{
+            if(totalLength.length > 17){
+                lower.innerText = total.toExponential(2)
+            }
+            else{
+                lower.innerText = total
+            }
+        }
+    }else{
+
+        if(totalLength.length > 17){
+            lower.innerText = total.toExponential(2)
+        }
+        else{
+            lower.innerText = total
+        }
+
+    }
+
+}
+
+let erase = () => {
+
+    if(calculation.length <= 1){
+        calculation.pop()
+        upper.innerText = 0
+    }else{
+        calculation.pop()
+        currentNumber = calculation.join("")
+        displayUpdate()
+    }
+}
+
+
+let eraseAll = () => {
+    calculation = []
+    currentNumber = 0
+    total = 0
+    currentOperator = ""
+    setZero()
+}
+
+let setZero = () => {
+
+    upper.innerText = 0
+    lower.innerText = ""
+}
+
 let solve = () => {
 
     if(total == 0){
-        total= currentNumber
-        
+        total= currentNumber  
     }else{
         if(currentOperator === '+'){
             total+= currentNumber;
@@ -152,4 +175,11 @@ let solve = () => {
         }
     }
     displayTotal()
+}
+
+let set = (operator) => {
+    currentOperator = operator
+    calculation = []
+    displayUpdate()
+    upper.innerText = ""
 }
